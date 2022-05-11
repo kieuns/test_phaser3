9de3bd680cc74527479b59108d547b0b4ff62e58
kieuns <kieuns@gmail.com>
2022-05-11 23:48:11 +0900

    점찍기 성공

--- CurveTestScene.js
+++ CurveTestScene.js
@@ -7,6 +7,21 @@ import { ResInfo } from "./res.js";
 // example
 // - geom : http://phaser.io/examples/v3/category/geom
 
+/*
+// ex1
+graphics.lineStyle(5, 0xFF00FF, 1.0);
+graphics.beginPath();
+graphics.moveTo(100, 100);
+graphics.lineTo(200, 200);
+graphics.closePath();
+graphics.strokePath();
+// ex2
+graphics.lineStyle(5, 0xFF00FF, 1.0);
+graphics.fillStyle(0xFFFFFF, 1.0);
+graphics.fillRect(50, 50, 400, 200);
+graphics.strokeRect(50, 50, 400, 200);
+*/
+
 export default class CurveTestScene extends Phaser.Scene
 {
     static instance = undefined;
@@ -36,13 +51,13 @@ export default class CurveTestScene extends Phaser.Scene
         this.graphics = this.add.graphics(); // this.add.graphics({ lineStyle: { width: 4, color: 0xaa00aa } });
 
         this.lineDataArr = [
-            { line:{color:0xffffff}, from:{x:0, y:0}, to:{x:100, y:200 } },
-            { line:{color:0xff0000}, from:{x:200, y:50}, to:{x:200, y:400} }
+            { fillStyle:{color:0xffffff, size:1, alpha:1.0 }, from:{x:0, y:0}, to:{x:100, y:200 } },
+            { fillStyle:{color:0xff0000, size:1, alpha:1.0 }, from:{x:200, y:50}, to:{x:200, y:400} }
         ];
         this.lineObjArr = [];
 
         this.dotDataArr = [
-            { line:{color:0xff00ff}, to:{x:100, y:200 } },
+            { fillStyle:{color:0xff00ff, size:10, alpha:1.0 }, to:{x:100, y:200 } },
         ];
         this.dotObjArr = [];
     }
@@ -81,22 +96,25 @@ export default class CurveTestScene extends Phaser.Scene
         for(let i = 0; i < this.lineDataArr.length; i++)
         {
             let line = this.getLine();
-            line.x1 = this.lineDataArr[i].from.x;
-            line.y1 = this.lineDataArr[i].from.y;
-            line.x2 = this.lineDataArr[i].to.x;
-            line.y2 = this.lineDataArr[i].to.y;
+            let spec = this.lineDataArr[i];
+            line.x1 = spec.from.x;
+            line.y1 = spec.from.y;
+            line.x2 = spec.to.x;
+            line.y2 = spec.to.y;
 
-            this.graphics.lineStyle(1, this.lineDataArr[i].line.color, 1);
+            this.graphics.lineStyle(spec.fillStyle.size, spec.fillStyle.color, spec.fillStyle.alpha);
             this.graphics.strokeLineShape(line);
         }
 
         for(let i = 0; i < this.dotDataArr.length; i++)
         {
             let dot = this.getDot();
-            dot.x = this.dotDataArr[i].to.x;
-            dot.y = this.dotDataArr[i].to.y;
+            let spec = this.dotDataArr[i];
+            dot.x = spec.to.x;
+            dot.y = spec.to.y;
 
-            this.graphics.fillPointShape(dot, 20);
+            this.graphics.fillStyle(spec.fillStyle.color, spec.fillStyle.alpha);
+            this.graphics.fillPointShape(dot, spec.fillStyle.size);
         }
     }
 };
