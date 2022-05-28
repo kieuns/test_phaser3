f2f39e70e229f06e22b8efcd6fd31189f26dabe5
kieuns <kieuns@gmail.com>
2022-05-28 13:40:48 +0900

    입력 부분 코드 정리

--- npm/src/TickTest.js
+++ npm/src/TickTest.js
@@ -447,23 +447,26 @@ export class TickTest extends Phaser.Scene
 
         this.input.on('pointerdown', this.onPointerDown.bind(this));
         this.input.on('pointerup', this.onPointerUp.bind(this));
-        this.input.on('gameout', this.onPointerUp); // canvas out
+        this.input.on('gameout', this.onCanvasOut.bind(this)); // canvas out
         this.input.on('pointermove', this.onPointerMove.bind(this));
 
         this.input.keyboard.on('keydown', (event) => {
             if(event.keyCode === Phaser.Input.Keyboard.KeyCodes.SHIFT) {
-                this._shiftKeyPressed = true; console.log('shift down');
+                this._shiftKeyPressed = true;
+                console.log('shift down');
             }
         });
         // 크롬에서 안되는데 왜지?
         this.input.keyboard.on('keyup', (event) => {
             if(event.keyCode === Phaser.Input.Keyboard.KeyCodes.SHIFT) {
-                this._shiftKeyPressed = false; console.log('shift up');
+                this._shiftKeyPressed = false;
+                console.log('shift up');
             }
         });
 
         this.graphics = this.add.graphics();
     }
+
 
     /**
      * @param {number} time - unit ms
@@ -482,95 +485,105 @@ export class TickTest extends Phaser.Scene
         this._objMov2.onMove(delta);
     }
 
-    cancelInputCapture()
-    {
-        this._mouseDown = false;
-        this._clickedLine = null;
-    }
 
     /** @param {Phaser.Input.Pointer} pointer */
     onPointerDown(pointer)
     {
-        if(!pointer.active) return;
+        // if(!pointer.active) return;
+
         let lbtn_down = pointer.leftButtonDown();
         let rbtn_down = pointer.rightButtonDown();
 
-        if(!this._mouseDown && lbtn_down && !rbtn_down)
-        {
-            this._mouseDown = true;
-            //console.log('onPointerDown', pointer);
-            this._clickedLine = new ClickedLine(this, pointer.x, pointer.y);
+        if(!this._mouseDown && lbtn_down && !rbtn_down) {
+            this.startMouseInputCapture(pointer.x, pointer.y);
         }
-        else if(this._mouseDown && rbtn_down)
-        {
-            this.cancelInputCapture();
+        else if(this._mouseDown && rbtn_down) {
+            this.cancelMouseInputCapture();
         }
     }
 
     /** @param {Phaser.Input.Pointer} pointer */
     onPointerMove(pointer)
     {
-        if(!pointer.active) {
-            if(this._clickedLine) {
-                this.cancelInputCapture();
-            }
-            return;
-        }
-        if(this._mouseDown && pointer.leftButtonDown())
-        {
-            if(this._clickedLine)
-            {
-                let x = pointer.x;
-                let y = pointer.y;
-                //if(this._shiftKeyPressed) {
-                if(true) {
-                    x = x - (pointer.x % 10);
-                    y = y - (pointer.y % 10);
-                }
-                this._clickedLine.updateEndPosition(x, y);
-            }
+        if(this._mouseDown && pointer.leftButtonDown()) {
+            this.updateMouseInputCapture(pointer.x, pointer.y);
         }
     }
 
     /** @param {Phaser.Input.Pointer} pointer */
     onPointerUp(pointer)
     {
-        if(!pointer.active) {
-            if(this._clickedLine || this._mouseDown) {
-                this.cancelInputCapture();
-            }
-            return;
-        }
         let lbtn_up = pointer.leftButtonReleased();
 
-        if(GameOption.log_detail()) { console.log('detail'); }
-
-        if(this._mouseDown && lbtn_up)
-        {
-            this._mouseDown = false;
-            //console.log('onPointerUp', pointer);
-            if(this._clickedLine) {
-                this._clickedLine.close();
-                this._clickLineArr.push(this._clickedLine);
-                let tm_line = this._clickedLine;
-                this._clickedLine = null;
-
-                this._objMov2.rotationSet(tm_line.rotationGet());
-                this._objMov2.moveSet2(tm_line.line.getPointA(), tm_line.line.getPointB(), 5, true, () => { console.log('landed'); });
-
-                this._tickPlay.reserveBy(5, (() => {
-                    if(GameOption.log_detail()) { console.log('ReserveWork : tick: ', this._tickPlay.getNowTick(), ', ', this._tickPlay.getNowAsTime()); }
-
-                    let idx = this._clickLineArr.findIndex((v, i, a) => v === tm_line);
-                    if(idx !== -1) {
-                        this._clickLineArr.splice(idx, 1);
-                        if(GameOption.log_detail()) { console.log('_clickLineArr : count : ', this._clickLineArr.length); }
-                    }
-                }).bind(this));
-            }
+        if(this._mouseDown && lbtn_up) {
+            this.closeMouseInputCapture(pointer.x, pointer.y);
         }
     }
 
+    /** 마우스가 캔바스를 벗어나면 호출
+     * @param {Phaser.Input.Pointer} pointer */
+    onCanvasOut(pointer)
+    {
+        if(this._clickedLine || this._mouseDown) {
+            this.cancelMouseInputCapture();
+        }
+    }
+
+
+
+    startMouseInputCapture(x, y)
+    {
+        this._mouseDown = true;
+        this._clickedLine = new ClickedLine(this, x, y);
+    }
+
+    cancelMouseInputCapture()
+    {
+        this._mouseDown = false;
+        this._clickedLine = null;
+    }
+
+    updateMouseInputCapture(pt_x, pt_y)
+    {
+        if(!this._clickedLine) { return; }
+        let x = pt_x;
+        let y = pt_y;
+        if(this._shiftKeyPressed)
+        {
+            // 첫번째 점 위치를 기준으로 변화값 구해서
+            let offset_x = pt_x - this._clickedLine.line.x1;
+            let offset_y = pt_y - this._clickedLine.line.y1;
+            // 새로운 좌표에서 10 이하 값을 뺀다.
+            x = pt_x - (offset_x % 20);
+            y = pt_y - (offset_y % 20);
+        }
+        this._clickedLine.updateEndPosition(x, y);
+    }
+
+    closeMouseInputCapture(ptx, pty)
+    {
+        this._mouseDown = false;
+
+        if(!this._clickedLine) { return; }
+
+        this._clickedLine.close();
+        this._clickLineArr.push(this._clickedLine);
+        let tm_line = this._clickedLine;
+        this._clickedLine = null;
+
+        this._objMov2.rotationSet(tm_line.rotationGet());
+        this._objMov2.moveSet2(tm_line.line.getPointA(), tm_line.line.getPointB(), 5, true, () => { console.log('landed'); });
+
+        this._tickPlay.reserveBy(5, (() => {
+            if(GameOption.log_detail()) { console.log('ReserveWork : tick: ', this._tickPlay.getNowTick(), ', ', this._tickPlay.getNowAsTime()); }
+
+            let idx = this._clickLineArr.findIndex((v, i, a) => v === tm_line);
+            if(idx !== -1) {
+                this._clickLineArr.splice(idx, 1);
+                if(GameOption.log_detail()) { console.log('_clickLineArr : count : ', this._clickLineArr.length); }
+            }
+        }).bind(this));
+    }
 }
 
 
