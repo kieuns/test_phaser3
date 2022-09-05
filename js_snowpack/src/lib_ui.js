af34befe1be28e482cf4c35c8c71fee3ab0f224e
문기은 <kieuns@gmail.com>
2022-09-05 21:57:48 +0900

    버튼 클래스 작업하려구 추가 해둠

--- js_snowpack/src/lib_ui.js
+++ js_snowpack/src/lib_ui.js
@@ -1,6 +1,8 @@
 
 import Phaser from 'phaser'
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 export class UI_TextButton
 {
     /** @type {Phaser.GameObjects.Text} */
@@ -18,8 +20,7 @@ export class UI_TextButton
      * @param {JSON} style
      * @param {function} onClickResponse
      */
-    constructor(scene, text, x, y, style, onClickResponse)
-    {
+    constructor(scene, text, x, y, style, onClickResponse) {
         this.init(scene, text, x, y, style, onClickResponse);
     }
 
@@ -55,17 +56,82 @@ export class UI_TextButton
         this._hiddenText.active = false;
     }
 
-    setPosition(x, y)
-    {
+    setPosition(x, y) {
         this._text.setPosition(x, y);
         this._hiddenText.setPosition(x, y);
     }
 
     /** @param {Function} onClickResponse */
-    setClickCallback(onClickResponse)
-    {
+    setClickCallback(onClickResponse) {
         this._onClick = onClickResponse;
     }
 }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+export class UI_Button
+{
+    /** @type {Phaser.GameObjects.Text} */
+    _text = null;
+    /** @type {Phaser.GameObjects.Text} */
+    _hiddenText = null;
+    /** @type {() => void} */
+    _onClick = null;
+
+    /**
+     * @param {Phaser.Scene} scene
+     * @param {string} text
+     * @param {number} x
+     * @param {number} y
+     * @param {JSON} style
+     * @param {function} onClickResponse
+     */
+    constructor(scene, text, x, y, style, onClickResponse)
+    {
+        this.init(scene, text, x, y, style, onClickResponse);
+    }
+
+    /**
+     * @param {Phaser.Scene} scene
+     * @param {string} text
+     * @param {number} x
+     * @param {number} y
+     * @param {JSON} style
+     * @param {function} onClickResponse
+     */
+    init(scene, text, x, y, style, onClickResponse)
+    {
+        x = x ? x : 0;
+        y = y ? y : 0;
+        style = style ? style : { color: '#00ff00' };
+
+        if(onClickResponse) {
+            this._onClick = onClickResponse;
+        }
+
+        this._text = scene.add.text(x, y, text);
+        this._text.setInteractive();
+
+        this._text.on('pointerdown', () =>
+        {
+            console.log('on click');
+            this._onClick && this._onClick();
+        });
+
+        this._hiddenText = scene.add.text(x, y, text);
+        this._hiddenText.active = false;
+    }
+
+    setPosition(x, y) {
+        this._text.setPosition(x, y);
+        this._hiddenText.setPosition(x, y);
+    }
+
+    /** @param {Function} onClickResponse */
+    setClickCallback(onClickResponse) {
+        this._onClick = onClickResponse;
+    }
+}
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
